25cdb924e804404c770a2385cfcc9b41e555bb79
Yu, Qihui <Qihui.Yu@misys.com>
2018-12-18 12:17:38 +0100

    Add Selection page and minor-triads page

---
+++ js/minor-triads.js
@@ -0,0 +1,93 @@
+var timer = null;
+var interval_value;
+
+// function start() {
+//     clearInterval(timer);
+//     var input_value = document.getElementById('interval').value;
+//     console.log("Ready...");
+//     var chordN, chordPrev;
+//     timer = setInterval(function(){
+//         do {
+//             chordN = Math.floor(Math.random() * 12);
+//         }
+//         while (chordN === chordPrev)
+//         // console.log(chordN);
+//         displayChord(chordN);
+//         chordPrev = chordN;
+//     }, input_value*1000 )
+// }
+
+
+var chordNames = [
+    'C','F','G','#F','A','D','E','bA','bD','bE','B','bB'
+]
+var chordPool = [];
+
+function prepareChordPool() {
+    var checks = document.querySelectorAll('#selectChords input[type="checkbox"]');
+    chordPool = [];
+    for(var i =0; i < checks.length-1; i++) {
+        var check = checks[i];
+        if(check.checked == true) {
+            chordPool.push(chordNames[i]);
+        }
+    }
+    console.log(chordPool)
+}
+
+function displayChord(chordN) {
+    document.getElementById('chordName').innerHTML = chordPool[chordN];
+    document.getElementById('chordName').style.fontSize = '255px';
+}
+
+function start() {
+    clearInterval(timer);
+    var radios = document.getElementsByName('interval');
+
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+            interval_value = radios[i].value;
+            break; 
+        }
+    }
+    prepareChordPool();
+
+    if (chordPool.length <=1 ) {
+        document.getElementById('chordName').innerHTML = " Please select more chords...";
+        document.getElementById('chordName').style.fontSize = '40px';
+        document.getElementById('chordName').style.textAlign = 'center';
+        return;
+    }
+
+    console.log("Ready...");
+    document.getElementById('chordName').innerHTML = "Ready...";
+    document.getElementById('chordName').style.fontSize = '80px';
+
+    var chordN, chordPrev;
+    timer = setInterval(function(){
+        do {
+            chordN = Math.floor(Math.random() * chordPool.length);
+        }
+        while (chordN === chordPrev)
+        // console.log(chordN);
+        displayChord(chordN);
+        chordPrev = chordN;
+    }, interval_value )
+}
+
+
+
+function toggleAllSelect(containerId) {
+    // All checkbox booleans
+    var checks = document.querySelectorAll('#' + containerId + ' input[type="checkbox"]');
+    // lastCheck value is the select/deselect all value
+    var lastCheck = checks[checks.length - 1];
+    for(var i =0; i< checks.length;i++) {
+        var check = checks[i];
+        if(!check.disabled) {
+            // Set all checkbox the same status as the last checkbox
+            check.checked = (lastCheck.checked == true) ? true : false;
+        }
+    }
+}
+
